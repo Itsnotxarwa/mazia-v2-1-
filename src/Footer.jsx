5506ab64e8a0f1a9bb7bf91b497880faa4a49844
itsnotxarwa <marwa.kchkh@gmail.com>
2026-01-21 15:03:28 +0100

    update linkedin link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -32,15 +32,15 @@ export default function Footer() {
                             </a>
     
                             <a 
-                            href="https://www.linkedin.com/company/Mazia-ai/" 
+                            href="https://l.instagram.com/?u=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fmaximize-infogerance%2Fposts%2F%3FfeedView%3Dall&e=AT0pH19kMekaMyfVuyVSkXz5xz8aNPA9ylMjH7a6FvEtHZMBfDTAJAZVIpKd1glIxryXb98KyEESVZ-NNKG-mWmShLGAST3I_rI2VRFqZ57OVip7juVmMzUwrOvO" 
                             target="_blank"
                             rel="noopener noreferrer"
                             className="socialContainer containerThree">
                                 <svg className="socialSvg linkdinSvg" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg>
                             </a>
-  
+
                             <a 
-                            href="https://www.facebook.com/share/17asGHYQMM/?mibextid=wwXIfr" 
+                            href="https://web.facebook.com/MaximizeInfogerance" 
                             target="_blank"
                             rel="noopener noreferrer"
                             className="socialContainer containerFour">
